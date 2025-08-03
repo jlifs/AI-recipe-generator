4fe7c7245cfeb098071f4c1381b5b32bd085ae49
jlifs <jlifs74@gmail.com>
2025-08-03 16:52:39 -0400

    Changed some styling and wording

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ function generateRecipe(event) {
 
   let apiKey = "38cc12b312104o45t5c1faaa8bf9b6c0";
   let context =
-    "You are a chef expert and you love to share you recipes. Your mission is to generate a quick easy recipe that can be made in 30 minutes or less. Give the recipe title only in <strong/> with font size of 24 pixels at the top then separated by <br/> and list ingredients in bullet format,all separated by <br/> and number instructions in the recipe, also all separated by <br/>. Be sure to include section headings of Ingredients and Instructions in <strong/> element, keep all other text normal and exclude the use of all asterisk. Use the international metric system. Make sure to follow user instructions. Then add <br/> element and sign the recipe with 'SheCodes AI' in a <strong/> element at the end.";
+    "You are an expert chef and you love to share your recipes. Your mission is to generate a quick easy recipe that can be made in 30 minutes or less. Give the recipe title only in <strong/> with font size of 24 pixels at the top then separated by <br/> and list ingredients in bullet format,all separated by <br/> and number instructions in the recipe, also all separated by <br/>. Be sure to include section headings of Ingredients and Instructions in <strong/> element, keep all other text normal and exclude the use of all asterisk. Use the American metric system. Make sure to follow user instructions. Then add <br/> element and sign the recipe with 'AI Recipes' in a <strong/> element at the end.";
   let prompt = `User instructions: Generate a detailed recipe about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
   recipeElement.classList.remove("hidden");
-  recipeElement.innerHTML = `<div class="generating">⏱️ Generating a ${instructionsInput.value} for you 🍲!</div>`;
+  recipeElement.innerHTML = `<div class="generating">⏱️ Generating a ${instructionsInput.value} recipe for you 🍲!</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
